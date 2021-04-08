7ea95fb0943ec0a9da893f8e29b43235190360ae
MintyMonster <78669960+MintyMonster@users.noreply.github.com>
2021-04-08 21:16:29 +0100

    Update MetroCore.java

--- src/main/java/com/minty/metrocore/MetroCore.java
+++ src/main/java/com/minty/metrocore/MetroCore.java
@@ -50,7 +50,7 @@ public class MetroCore extends JavaPlugin {
     public void onEnable() {
         getServer().getPluginManager().registerEvents(new MetroCoreListener(), this);
         try {
-            jda = JDABuilder.createDefault("ODI5MDIzNTEzNDIzMTE4MzY2.YGyGUA.MrjefqDDIAtrjeQe92yG3n8yGns").build();
+            jda = JDABuilder.createDefault("token").build();
             jda.addEventListener(new DiscordListener());
             jda.awaitReady();
             this.getLogger().info("[MetroCore] Connected to Discord.");
