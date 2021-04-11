07546e955885609309f400d18c233126d13abe7b
MintyMonster <camerongliddell@gmail.com>
2021-04-11 04:11:45 +0100

    Changing lots of stuffs

--- src/main/java/com/minty/metrocore/MetroCore.java
+++ src/main/java/com/minty/metrocore/MetroCore.java
@@ -1,13 +1,10 @@
 package com.minty.metrocore;
 
-import commands.*;
-import filehandling.CreateAdminFile;
-import filehandling.CreateModFile;
-import filehandling.LogCommands;
-import filehandling.PlayerJoinDatabase;
-import listeners.*;
-import methods.AdminMode;
-import methods.ModMode;
+import com.minty.metrocore.commands.*;
+import com.minty.metrocore.filehandling.*;
+import com.minty.metrocore.listeners.*;
+import com.minty.metrocore.methods.AdminMode;
+import com.minty.metrocore.methods.ModMode;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import org.bukkit.*;
@@ -16,27 +13,26 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import javax.security.auth.login.LoginException;
-import java.sql.*;
 
-import java.io.*;
-import java.time.LocalDateTime;
 import java.util.*;
 
 public class MetroCore extends JavaPlugin {
 
-    private static Connection c = null;
-    private static Statement stmt = null;
     private final static ConsoleCommandSender sender = Bukkit.getConsoleSender();
     public static HashMap<Player, Boolean> Mod;
     public static HashMap<Player, Boolean> Admin;
     public static JDA jda;
-    private PlayerStates playerStates;
+    //private final PlayerStates playerStates;
     private PlayerJoinDatabase playerDatabase;
     private CreateAdminFile adminFile;
     private CreateModFile modFile;
     private LogCommands logCommands;
     private ModMode modMode;
     private AdminMode adminMode;
+    private CreateDirectory createDirectory;
+    private CreatePluginFiles createPluginFiles;
+    private CreateDatabase createDatabase;
+
     // /yeet remake huehuehue
     // DiscordBot from server > bugreports || punishments  || player reports || ranks++ (Discord -> Minecraft <- Sync) || suggestions
     // /punish > GUI with selective options > Follows guidelines > Takes previous offenses into account too
@@ -49,7 +45,7 @@ public class MetroCore extends JavaPlugin {
     public void onEnable() {
 
         try {
-            jda = JDABuilder.createDefault("token").build();
+            jda = JDABuilder.createDefault("ODI5MDIzNTEzNDIzMTE4MzY2.YGyGUA.q_SXA-dbRFJbeIMmm1W8e8mWpRo").build();
             //jda.addEventListener(new DiscordListener());
             jda.awaitReady();
             this.getLogger().info("[MetroCore] Connected to Discord.");
@@ -58,14 +54,15 @@ public class MetroCore extends JavaPlugin {
             this.getLogger().severe("[MetroCore] Cannot connect to Discord");
         }
 
-
-        this.playerStates = new PlayerStates();
         this.playerDatabase = new PlayerJoinDatabase(this);
         this.adminFile = new CreateAdminFile(this);
         this.modFile = new CreateModFile(this);
         this.logCommands = new LogCommands(this, adminFile, modFile);
         this.modMode = new ModMode(this);
         this.adminMode = new AdminMode(this);
+        this.createDirectory = new CreateDirectory(this);
+        this.createPluginFiles = new CreatePluginFiles(this, createDirectory);
+        this.createDatabase = new CreateDatabase(this, createDirectory);
 
         getServer().getPluginManager().registerEvents(new LandClaimListener(this), this);
         getServer().getPluginManager().registerEvents(new PlayerJoinListener(this, playerDatabase, adminFile, modFile), this);
@@ -89,125 +86,24 @@ public class MetroCore extends JavaPlugin {
         Mod = new HashMap<>();
         Admin = new HashMap<>();
         this.saveDefaultConfig();
-        File dir = this.getDataFolder();
-        File Folder = new File(dir + File.separator + "Logs" + File.separator);
-        File aidanFolder = new File(dir + File.separator + "AidanNotes" + File.separator);
-        File bugFolder = new File(dir + File.separator + "BugReports" + File.separator);
-        File reportFolder = new File(dir + File.separator + "PlayerReports" + File.separator);
-        File playerDataFolder = new File(dir + File.separator + "PlayerData" + File.separator);
-        if (!playerDataFolder.exists()) playerDataFolder.mkdir();
-        if (!bugFolder.exists()) bugFolder.mkdir();
-        if (!aidanFolder.exists()) aidanFolder.mkdir();
-        if (!Folder.exists()) Folder.mkdir();
-        if (!reportFolder.exists()) reportFolder.mkdir();
 
-        File playerData = new File(playerDataFolder, "PlayerData.db");
-        if (!playerData.exists()) {
-            try {
-                getServer().getLogger().info("[MetroCore] Missing PlayerData database, creating new database.");
-                playerData.createNewFile();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
+        createDirectory.createDir("Logs");
+        createDirectory.createDir("AidanNotes");
+        createDirectory.createDir("BugReports");
+        createDirectory.createDir("PlayerReports");
+        createDirectory.createDir("PlayerData");
 
-        try {
-            Class.forName("org.sqlite.JDBC");
-            String path = playerData.getPath();
-            c = DriverManager.getConnection("jdbc:sqlite:" + path);
+        createPluginFiles.createFile("PlayerData.db", "PlayerData");
+        createPluginFiles.createFile("AidanNotes.db", "AidanNotes");
+        createPluginFiles.createFile("BugReports.db", "BugReports");
+        createPluginFiles.createFile("PlayerReports.db", "PlayerReports");
 
-            stmt = c.createStatement();
-            String sql = "CREATE TABLE IF NOT EXISTS PLAYERDATA (ID INTEGER PRIMARY KEY NOT NULL, NAME TEXT NOT NULL, UUID TEXT NOT NULL, IP TEXT NOT NULL, DATE TEXT NOT NULL)";
-            stmt.executeUpdate(sql);
-            stmt.close();
-            c.close();
-            getServer().getLogger().info("[MetroCore] Finished creating new PlayerData database");
-
-        } catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
-            getServer().getLogger().severe("[MetroCore] PlayerData Database not created, please contact Triobros");
-        }
-
-        File file = new File(aidanFolder, "AidanNotes.db");
-        if (!file.exists()) {
-            try {
-                getServer().getLogger().info("[MetroCore] Missing AidanNotes database, creating new database.");
-                file.createNewFile();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-        try {
-            Class.forName("org.sqlite.JDBC");
-            String path = file.getPath();
-            c = DriverManager.getConnection("jdbc:sqlite:" + path);
-
-            stmt = c.createStatement();
-            String sql = "CREATE TABLE IF NOT EXISTS AIDANNOTES (ID INTEGER PRIMARY KEY NOT NULL, NOTE TEXT NOT NULL, STATUS TEXT NOT NULL)";
-            stmt.executeUpdate(sql);
-            stmt.close();
-            c.close();
-            getServer().getLogger().info("[MetroCore] Finished creating new AidanNotes database");
-
-        } catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
-            getServer().getLogger().severe("[MetroCore] AidanNotes Database not created, please contact Triobros");
-        }
-
-        File bugJson = new File(bugFolder, "BugReports.db");
-        if (!bugJson.exists()) {
-            try {
-                getServer().getLogger().info("[MetroCore] Missing BugReports database, creating new database.");
-                bugJson.createNewFile();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-        try {
-            Class.forName("org.sqlite.JDBC");
-            String path = bugJson.getPath();
-            c = DriverManager.getConnection("jdbc:sqlite:" + path);
-
-            stmt = c.createStatement();
-            String sql = "CREATE TABLE IF NOT EXISTS BUGREPORTS (ID INTEGER PRIMARY KEY NOT NULL, NAME TEXT NOT NULL, REPORT TEXT NOT NULL, STATUS TEXT NOT NULL)";
-
-            stmt.executeUpdate(sql);
-            stmt.close();
-            c.close();
-            getServer().getLogger().info("[MetroCore] Finished creating new BugReports database");
-
-        } catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
-            getServer().getLogger().severe("[MetroCore] BugReports Database not created, please contact Triobros");
-        }
-
-        File reportDb = new File(reportFolder, "PlayerReports.db");
-        if (!reportDb.exists()) {
-            try {
-                getServer().getLogger().info("[MetroCore] Missing PlayerReports database, creating new database");
-                reportDb.createNewFile();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-        try {
-            Class.forName("org.sqlite.JDBC");
-            String path = reportDb.getPath();
-            c = DriverManager.getConnection("jdbc:sqlite:" + path);
-
-            stmt = c.createStatement();
-            String sql = "CREATE TABLE IF NOT EXISTS PLAYERREPORTS (ID INTEGER PRIMARY KEY NOT NULL, PLAYER TEXT NOT NULL, REPORTED TEXT NOT NULL, REASON TEXT NOT NULL, DATE TEXT NOT NULL)";
-            stmt.executeUpdate(sql);
-            stmt.close();
-            c.close();
-            getServer().getLogger().info("[MetroCore] Finished creating new PlayerReports database");
-        } catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
-            getServer().getLogger().severe("[MetroCore] PlayerReports Database not created, please contact Triobros");
-        }
+        String sql = "CREATE TABLE IF NOT EXISTS PLAYERDATA (ID INTEGER PRIMARY KEY NOT NULL, NAME TEXT NOT NULL, UUID TEXT NOT NULL, IP TEXT NOT NULL, DATE TEXT NOT NULL)";
+        createDatabase.createDb("PlayerData", "PlayerData.db", sql);
+        sql = "CREATE TABLE IF NOT EXISTS BUGREPORTS (ID INTEGER PRIMARY KEY NOT NULL, NAME TEXT NOT NULL, REPORT TEXT NOT NULL, STATUS TEXT NOT NULL)";
+        createDatabase.createDb("BugReports", "BugReports.db", sql);
+        sql = "CREATE TABLE IF NOT EXISTS PLAYERREPORTS (ID INTEGER PRIMARY KEY NOT NULL, PLAYER TEXT NOT NULL, REPORTED TEXT NOT NULL, REASON TEXT NOT NULL, DATE TEXT NOT NULL)";
+        createDatabase.createDb("PlayerReports", "PlayerReports.db", sql);
     }
 
     @Override
